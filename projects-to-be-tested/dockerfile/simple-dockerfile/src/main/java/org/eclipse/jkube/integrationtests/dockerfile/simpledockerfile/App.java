1ab2875bbf47f10504a49f3b514b6967e19b9d0d
Rohan Kumar <rohaan@redhat.com>
2020-05-26 14:22:40 +0530

    Added Integration test for simple mode Dockerfile

---
+++ projects-to-be-tested/dockerfile/simple-dockerfile/src/main/java/org/eclipse/jkube/integrationtests/dockerfile/simpledockerfile/App.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.integrationtests.dockerfile.simpledockerfile;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+@RestController
+public class App {
+
+  @RequestMapping(value = "/")
+  public String index() {
+    return "Hello world!";
+  }
+}
