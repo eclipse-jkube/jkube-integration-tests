06e9004d3bcee563ca4c6c87959750c4fb3f4f50
JF Denise <jdenise@redhat.com>
2020-07-20 13:12:54 +0200

    Fix for issue eclipse#283, Add support for WildFly Bootable JAR, it tests. Signed-off-by: JF Denise <jdenise@redhat.com>

---
+++ it/src/test/java/org/eclipse/jkube/integrationtests/wildfly/jar/WildflyJar.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.integrationtests.wildfly.jar;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import org.eclipse.jkube.integrationtests.JKubeCase;
+import org.eclipse.jkube.integrationtests.maven.BaseMavenCase;
+
+import static org.eclipse.jkube.integrationtests.assertions.PodAssertion.assertPod;
+import static org.eclipse.jkube.integrationtests.assertions.PodAssertion.awaitPod;
+import static org.eclipse.jkube.integrationtests.assertions.ServiceAssertion.awaitService;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+abstract class WildflyJar extends BaseMavenCase implements JKubeCase {
+
+  static final String PROJECT_WILDFLY_JAR = "projects-to-be-tested/wildfly-jar/microprofile";
+
+  @Override
+  public String getProject() {
+    return PROJECT_WILDFLY_JAR;
+  }
+
+  @Override
+  public String getApplication() {
+    return "wildfly-jar-microprofile";
+  }
+
+  final Pod assertThatShouldApplyResources() throws Exception {
+    final Pod pod = awaitPod(this).getKubernetesResource();
+    assertPod(pod).apply(this)
+      .logContains("wildfly-jar-microprofile-0.0.0-SNAPSHOT-wildfly.jar", 15)
+      .logContains("Deployed \"wildfly-jar-microprofile-0.0.0-SNAPSHOT.war\"", 60)
+      .logContains("WFLYSRV0025", 15);
+    awaitService(this, pod.getMetadata().getNamespace())
+      .assertIsNodePort()
+      .assertExposed()
+      .assertPorts(hasSize(1))
+      .assertPort("http", 8080, true)
+      .assertNodePortResponse("http", equalTo("JKube from WildFly JAR rocks!"));
+    return pod;
+  }
+
+}
---
+++ it/src/test/java/org/eclipse/jkube/integrationtests/wildfly/jar/WildflyJarK8sITCase.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.integrationtests.wildfly.jar;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.parallel.ResourceLock;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import static org.eclipse.jkube.integrationtests.Locks.CLUSTER_RESOURCE_INTENSIVE;
+import static org.eclipse.jkube.integrationtests.Tags.KUBERNETES;
+import static org.eclipse.jkube.integrationtests.assertions.DeploymentAssertion.assertDeploymentExists;
+import static org.eclipse.jkube.integrationtests.assertions.DeploymentAssertion.awaitDeployment;
+import static org.eclipse.jkube.integrationtests.assertions.DockerAssertion.assertImageWasRecentlyBuilt;
+import static org.eclipse.jkube.integrationtests.assertions.YamlAssertion.yaml;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
+
+@Tag(KUBERNETES)
+@TestMethodOrder(OrderAnnotation.class)
+class WildflyJarK8sITCase extends WildflyJar {
+
+  private KubernetesClient k;
+
+  @BeforeEach
+  void setUp() {
+    k = new DefaultKubernetesClient();
+  }
+
+  @AfterEach
+  void tearDown() {
+    k.close();
+    k = null;
+  }
+
+  @Override
+  public KubernetesClient getKubernetesClient() {
+    return k;
+  }
+
+  @Override
+  protected List<String> getProfiles() {
+    return Collections.singletonList(KUBERNETES);
+  }
+
+  @Test
+  @Order(1)
+  @DisplayName("k8s:build, should create image")
+  void k8sBuild() throws Exception {
+    // When
+    final InvocationResult invocationResult = maven("k8s:build");
+    // Then
+    assertThat(invocationResult.getExitCode(), Matchers.equalTo(0));
+    assertImageWasRecentlyBuilt("integration-tests", "wildfly-jar-microprofile");
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("k8s:resource, should create manifests")
+  void k8sResource() throws Exception {
+    // When
+    final InvocationResult invocationResult = maven("k8s:resource");
+    // Then
+    assertThat(invocationResult.getExitCode(), Matchers.equalTo(0));
+    final File metaInfDirectory = new File(
+      String.format("../%s/target/classes/META-INF", PROJECT_WILDFLY_JAR));
+    assertThat(metaInfDirectory.exists(), equalTo(true));
+    assertListResource(new File(metaInfDirectory, "jkube/kubernetes.yml"));
+    assertThat(new File(metaInfDirectory, "jkube/kubernetes/wildfly-jar-microprofile-deployment.yml"), yaml(not(anEmptyMap())));
+    assertThat(new File(metaInfDirectory, "jkube/kubernetes/wildfly-jar-microprofile-service.yml"), yaml(not(anEmptyMap())));
+  }
+
+  @Test
+  @Order(3)
+  @ResourceLock(value = CLUSTER_RESOURCE_INTENSIVE, mode = READ_WRITE)
+  @DisplayName("k8s:apply, should deploy pod and service")
+  @SuppressWarnings("unchecked")
+  void k8sApply() throws Exception {
+    // When
+    final InvocationResult invocationResult = maven("k8s:apply");
+    // Then
+    assertThat(invocationResult.getExitCode(), Matchers.equalTo(0));
+    final Pod pod = assertThatShouldApplyResources();
+    awaitDeployment(this, pod.getMetadata().getNamespace())
+      .assertReplicas(equalTo(1))
+      .assertContainers(hasSize(1))
+      .assertContainers(hasItems(allOf(
+        hasProperty("image", equalTo("integration-tests/wildfly-jar-microprofile:latest")),
+        hasProperty("name", equalTo("wildfly-jar")),
+        hasProperty("ports", hasSize(3)),
+        hasProperty("ports", hasItems(allOf(
+          hasProperty("name", equalTo("http")),
+          hasProperty("containerPort", equalTo(8080))
+        ))),
+        hasProperty("env", hasItems(allOf(
+          hasProperty("name", equalTo("JAVA_OPTIONS")),
+          hasProperty("value", equalTo("-Djava.net.preferIPv4Stack=true"))
+        ))),
+        hasProperty("env", hasItems(allOf(
+          hasProperty("name", equalTo("JAVA_ARGS")),
+          hasProperty("value", equalTo("-Djboss.bind.address=0.0.0.0"))
+        )))
+      )));
+  }
+
+  @Test
+  @Order(4)
+  @DisplayName("k8s:undeploy, should delete all applied resources")
+  void k8sUndeploy() throws Exception {
+    // When
+    final InvocationResult invocationResult = maven("k8s:undeploy");
+    // Then
+    assertThat(invocationResult.getExitCode(), Matchers.equalTo(0));
+    assertThatShouldDeleteAllAppliedResources(this);
+    assertDeploymentExists(this, equalTo(false));
+  }
+}
