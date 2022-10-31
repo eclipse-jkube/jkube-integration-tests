c0d30eb2114f0ef57ab03e68084356bd0c875218
Marc Nuri <marc@marcnuri.com>
2022-10-31 06:52:22 +0100

    refactor: pod assertions use AsyncUtil

--- it/src/main/java/org/eclipse/jkube/integrationtests/assertions/JKubeAssertions.java
+++ it/src/main/java/org/eclipse/jkube/integrationtests/assertions/JKubeAssertions.java
@@ -16,18 +16,17 @@ package org.eclipse.jkube.integrationtests.assertions;
 import io.fabric8.kubernetes.api.model.Pod;
 import org.eclipse.jkube.integrationtests.JKubeCase;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.eclipse.jkube.integrationtests.AsyncUtil.await;
 
 public class JKubeAssertions {
 
   private static final int DEFAULT_TIMEOUT_SECONDS = 10;
-  private static final int MAX_RETRIES = 10;
 
   private final JKubeCase jKubeCase;
 
@@ -47,12 +46,14 @@ public class JKubeAssertions {
       .findAny();
     final UnaryOperator<Pod> refreshPod = pod ->
       jKubeCase.getKubernetesClient().pods().withName(pod.getMetadata().getName()).fromServer().get();
-    final boolean podIsStillRunning = retryWhileTrue(() -> matchingPod.map(refreshPod)
-      .filter(updatedPod -> updatedPod.getMetadata().getDeletionTimestamp() == null)
-      .isPresent()
-    );
-    assertThat("Pod is still running when it should have been deleted",
-      podIsStillRunning, equalTo(false));
+    try {
+      await(() -> matchingPod.map(refreshPod))
+        .apply(p -> p.isEmpty() || p.get().getMetadata().getDeletionTimestamp() != null)
+        .get(DEFAULT_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+    } catch (TimeoutException | ExecutionException e) {
+      throw new AssertionError("Pod for application '" +
+        jKubeCase.getApplication() + "' is still running when it should have been deleted");
+    }
     return this;
   }
 
@@ -79,17 +80,4 @@ public class JKubeAssertions {
   public JKubeAssertions assertThatShouldDeleteAllAppliedResources() throws InterruptedException {
     return assertPodDeleted().assertServiceDeleted();
   }
-
-    private static boolean retryWhileTrue(Supplier<Boolean> func) throws InterruptedException {
-      boolean ret = true;
-      int current = 0;
-      while (current++ < MAX_RETRIES) {
-        ret = func.get();
-        if (!ret) {
-          break;
-        }
-        Thread.sleep(300L);
-      }
-      return ret;
-    }
 }
