3fad41e40aeed6fa8051a373623ea3ad2133f6fe
Marc Nuri <marc@marcnuri.com>
2023-01-23 11:14:57 +0100

    refactor: ReportExtension used explicitly

---
+++ it/src/main/java/org/eclipse/jkube/integrationtests/jupiter/api/Report.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.integrationtests.jupiter.api;
+
+import org.eclipse.jkube.integrationtests.jupiter.api.extension.ReportExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ElementType.TYPE})
+@Retention(RUNTIME)
+@ExtendWith({ReportExtension.class})
+public @interface Report {
+}
